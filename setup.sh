cebec84f66fad9078c802056840273456d902c51
Brian Dailey <brian@dailytechnology.net>
2020-08-27 22:06:54 -0500

    cattype

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #/usr/bin/env bash
+cd ~/.dotfiles
+git submodule init && git submodule update
+cd ~
 rm ~/.bashrc ~/.psqlrc ~/.gitconfig ~/.vimrc ~/.vim ~/.tmux.conf ~/.tmux.conf.local
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/psqlrc ~/.psqlrc
@@ -7,3 +10,5 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/tmux.conf.local ~/.tmux.conf.local
+
+echo "Remember to open vim and run :PlugInstall."
