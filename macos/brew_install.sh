ed7ae5d52caacfb4725a2dc14e3881e9d533e00d
Brian Dailey <brian@creollc.co>
2025-08-26 10:28:18 -0500

    Start tracking what we like to install with brew.

---
+++ macos/brew_install.sh
@@ -0,0 +1,10 @@
+brew install \
+# https://github.com/sharkdp/bat
+bat \
+# https://github.com/BurntSushi/ripgrep
+ripgrep \
+# https://github.com/starship/starship
+starship \
+# https://github.com/muesli/duf
+duf \
+git
